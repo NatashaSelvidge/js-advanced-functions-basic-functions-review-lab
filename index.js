1ea6e11dfcd140c21f76b3d604564e8585fdfc79
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-03-03 11:23:28 -0800

    commit tree 8f777ea5c048ad80cabc7a3f98657d0b69f316b6

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(string = "roller-skate") {
+  return `This Saturday, I want to ${string}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(string = "go to the office") {
+  return `This Monday, I will ${string}.`;
+};
+
+mondayWork();
+mondayWork("I will work from home");
+
+function wrapAdjective(symbol="*"){
+return function(adjective="special"){
+  return `You are ${symbol}${adjective}${symbol}!`;
+  }
+}
+
+const Calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b
+}
+
+function actionApplyer(start, array){
+  for (let i = 0; i<  array.length; i++){
+    start = array[i](start);
+  }
+  return start;
+}
